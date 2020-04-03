48555bc433c70507c89f93f2358b40a635196fec
Ash <ishans632@gmail.com>
2020-04-03 19:50:22 +0530

    Updates

---
+++ precache-manifest.b941d9501c46ebbe6453eb143f18ecbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fc4bba51d2b1621671f91d0f5db824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b6fd8c22dccc9cf6c85",
+    "url": "/static/css/main.d1f420d2.chunk.css"
+  },
+  {
+    "revision": "d1fcc82ac1359861a881",
+    "url": "/static/js/2.7ee7f346.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ee7f346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6fd8c22dccc9cf6c85",
+    "url": "/static/js/main.d8e1095f.chunk.js"
+  },
+  {
+    "revision": "6fd86fc0a203b0021eb9",
+    "url": "/static/js/runtime-main.4959d692.js"
+  }
+]);
